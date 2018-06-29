563abe8480dcd70e73d47e1477dcf6b3e6e28543
latilanoh@gmail.com <latilanoh@gmail.com>
2018-06-28 22:27:33 -0500

    se reformulan las funciones para habilitar el login con gmail

--- js/index.js
+++ js/index.js
@@ -1,18 +1,66 @@
 var config = {
-    apiKey: "AIzaSyDj_C75Jq1mQpeBNjSGEpJPLhjSIm_-IFE",
-    authDomain: "entre-t.firebaseapp.com",
-    databaseURL: "https://entre-t.firebaseio.com",
-    projectId: "entre-t",
-    storageBucket: "entre-t.appspot.com",
-    messagingSenderId: "1017925579785"
+    apiKey: "AIzaSyBA8vnDbnmq0pZqljlRlBdwe1rE6BTjXY0",
+    authDomain: "entre-t-2.firebaseapp.com",
+    databaseURL: "https://entre-t-2.firebaseio.com",
+    projectId: "entre-t-2",
+    storageBucket: "entre-t-2.appspot.com",
+    messagingSenderId: "499672803740"
 };
 
 firebase.initializeApp(config);
 
-/* Variable global con la información del usuario */
-var userInfo = {};
+$(document).ready(function(){
+    console.log('Pagina cargada');
 
-/* Función para guardar la info de los usuarios */
+    $('#btn-signup').click(function(){
+        console.log('boton clickeado signup');
+
+        $('#modal-SignUp').modal('show');
+
+        //Falta corregir esta parte para que se guarde la info correctamente de
+        //nvos usuarios en firebase
+        var email = $('#input-email').val();
+        var password = $('#input-password').val();
+        console.log(email,password);
+
+        firebase.auth().signInWithEmailAndPassword(email,password)
+        .catch( function(error) {
+            //alert(error.message);
+        });
+    });
+
+    $("#btn-login").click(function(){
+        //console.log('login')
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function(result){
+            saveUser(result.user);
+            //$('#photo-user').append("<img src='" + result.user.photoURL + "' />");
+            //$('.user-name').html('Lourdes Atilano');
+            //$('.user-name').append(result.user.displayName);
+            console.log(result.user.displayName);
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    
+    });
+
+});
+
+
+
+/* Variable global con la información del usuario 
+var userInfo = {}; */
+
+/* Función para guardar la info de los usuarios 
 function saveUser(user) {
     var userInfo = {
         uid: user.uid,
@@ -21,12 +69,13 @@ function saveUser(user) {
     };
 
    firebase.database().ref("users/" + user.uid).set(userInfo);
-};
-    
+}; */
+
+/*
 var provider = new firebase.auth.GoogleAuthProvider();
 
 $("btn-login").click(function(){
-    //console.log('login');
+    console.log('login');
     firebase.auth().signInWithPopup(provider).then(function(result){
         saveUser(result.user);
         //$('#photo-user').append("<img src='" + result.user.photoURL + "' />");
@@ -48,7 +97,7 @@ $("btn-login").click(function(){
 });
 
 document.getElementById("btn-logout").addEventListener('click',function (){
-    //console.log('logout');
+    console.log('logout');
     firebase.auth().signOut();
 });
 
@@ -58,17 +107,19 @@ firebase.auth().onAuthStateChanged(function(user) {
     } else {
     console.log('desloggeado');
     }
-});
+});*/
 
+/*
 $(document).ready(function () {
 
-    // // ******************FUNCION PARA SPLASH******************//
+    // ******************FUNCION PARA SPLASH******************
     // setTimeout(function () {
     //     $('#splash').fadeOut(500);
     // }, 1000);
-});    
+});   */
 
+/*
 document.getElementById("btn-signup").addEventListener('click',function(){
     //console.log("sign up");
     $('#modal-SignUp').modal('show');
-});
+});*/
